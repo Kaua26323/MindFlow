5b949f6f8a03227d3ba731e4fa32327a3a216b29
Kauã Souza <kaua26323@gmail.com>
2026-05-01 12:58:27 -0400

    fix(schema): make user_id mandatory in posts table

--- src/infra/db/drizzle/schemas/posts.schema.ts
+++ src/infra/db/drizzle/schemas/posts.schema.ts
@@ -4,9 +4,11 @@ import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core';
 export const postsTable = pgTable('posts', {
   id: uuid('id').primaryKey().defaultRandom(),
   text: text('text').notNull(),
-  user_id: uuid('user_id').references(() => usersTable.id, {
-    onDelete: 'cascade',
-  }),
+  user_id: uuid('user_id')
+    .references(() => usersTable.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updated_at: timestamp('updated_at'),
 });
