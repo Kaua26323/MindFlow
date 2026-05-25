a6da8bde73009720e55a0ee4ed0b7c3d2cb6d791
Kauã Souza <kaua26323@gmail.com>
2026-05-25 11:50:06 -0400

    feat(repo): add pagination and sorting to favorites repository contract

--- src/application/repositories/favorites.repository.ts
+++ src/application/repositories/favorites.repository.ts
@@ -1,12 +1,36 @@
-import type {
-  NewFavoritePost,
-  FavoritePost,
-} from '@/infra/db/drizzle/schemas/favoritesPosts.schema.ts';
-
-interface FavoritesPostsRepository {
-  addPost(data: NewFavoritePost): Promise<FavoritePost | null>;
-  removePost(data: NewFavoritePost): Promise<FavoritePost | null>;
-  getFavoritesPosts(userID: string): Promise<FavoritePost[] | []>;
+export interface FavoritePostDTO {
+  user_id: string;
+  post_id: string;
 }
 
-export type { FavoritesPostsRepository };
+export interface FavoritePost {
+  user_id: string;
+  post_id: string;
+  createdAt: Date;
+}
+
+export interface Result {
+  text: string;
+  user_id: string;
+  post_id: string;
+  createdAt: Date;
+}
+
+export interface SearchParams {
+  page: number;
+  order: 'DESC' | 'ASC';
+}
+
+export interface PaginatedFavorite<T> {
+  data: T[];
+  total: number;
+}
+
+export interface FavoritesPostsRepository {
+  getFavoritesPosts(
+    userID: string,
+    data: SearchParams,
+  ): Promise<PaginatedFavorite<Result>>;
+  addPost(data: FavoritePostDTO): Promise<FavoritePost | null>;
+  removePost(data: FavoritePostDTO): Promise<FavoritePost | null>;
+}
