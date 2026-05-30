17a03b41941136b430e69bc3b6eb9733bee6edb4
Kauã Souza <kaua26323@gmail.com>
2026-05-29 22:01:02 -0400

    feat(infra): implement findById in drizzle-user repository
    - Updated methods to use CreateUserDTO and pure User types
    
    - Update Drizzle repository data types

--- src/infra/db/repositories/drizzle-user.repository.ts
+++ src/infra/db/repositories/drizzle-user.repository.ts
@@ -1,9 +1,12 @@
+import { eq } from 'drizzle-orm';
 import { usersTable } from '@/infra/db/drizzle/schemas/users.schema.ts';
 
-import type { NewUser, User } from '../drizzle/schemas/users.schema.ts';
 import type { DrizzleDatabase } from '@/infra/db/drizzle/connection.ts';
 import type { UserRepository } from '@/application/repositories/user.repository.ts';
-import { eq } from 'drizzle-orm';
+import type {
+  User,
+  CreateUserDTO,
+} from '@/application/repositories/user.repository.ts';
 
 export class DrizzleUserRepository implements UserRepository {
   private readonly db: DrizzleDatabase;
@@ -12,11 +15,8 @@ export class DrizzleUserRepository implements UserRepository {
     this.db = db;
   }
 
-  async create(userData: NewUser): Promise<User | null> {
-    const [newUser] = await this.db
-      .insert(usersTable)
-      .values(userData)
-      .returning();
+  async create(userData: CreateUserDTO): Promise<User | null> {
+    const [newUser] = await this.db.insert(usersTable).values(userData).returning();
 
     return newUser ?? null;
   }
@@ -28,6 +28,14 @@ export class DrizzleUserRepository implements UserRepository {
       .returning();
 
     return deletedUser ?? null;
+  }
+
+  async findById(userID: string): Promise<User | null> {
+    const user = await this.db.query.usersTable.findFirst({
+      where: (user, { eq }) => eq(user.id, userID),
+    });
+
+    return user ?? null;
   }
 
   async findByEmail(email: string): Promise<User | null> {
